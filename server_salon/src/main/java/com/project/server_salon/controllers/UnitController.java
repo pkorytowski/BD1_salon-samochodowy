9b3e0a809bba4789519c9e31a9a145ebc75b846d
Paweł Korytowski <pkorytowski@hotmail.com>
2021-01-06 23:47:28 +0100

    Added addUnit and delete methods

--- server_salon/src/main/java/com/project/server_salon/controllers/UnitController.java
+++ server_salon/src/main/java/com/project/server_salon/controllers/UnitController.java
@@ -4,13 +4,12 @@ import com.project.server_salon.objects.Unit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Objects;
 
 @RequestMapping(path ="/units")
@@ -63,6 +62,93 @@ public class UnitController {
         return units;
     }
 
+    @PostMapping("/add")
+    public void addUnit(@RequestBody Map<String, String> request){
+        int id_car, id_color;
+        String status;
+
+        try{
+            id_car = Integer.parseInt(request.get("id_car"));
+            id_color = Integer.parseInt(request.get("id_color"));
+            status = request.get("status");
+        }
+        catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad request form");
+        }
+        try{
+            if(!getConn()){
+                throw new Exception();
+            }
+            PreparedStatement stmt = c.prepareStatement("INSERT INTO salon.egzemplarz values (default, ?, ?, ?, 0)");
+            stmt.setInt(1, id_car);
+            stmt.setInt(2, id_color);
+            stmt.setString(3, status);
+            int i=stmt.executeUpdate();
+            if(i!=1){
+                throw new ResponseStatusException(HttpStatus.CONFLICT, "Unit already exists");
+            }
+        }
+        catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Problem with connection with db");
+        }
+    }
+
+    @PostMapping("/delete")
+    public void delete(@RequestBody Map<String, String> request){
+
+        int id_unit;
+        try{
+            id_unit = Integer.parseInt(request.get("id_unit"));
+        }
+        catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad shape of request" + e.getMessage());
+        }
+
+        try{
+            if(!getConn()){
+                throw new Exception();
+            }
+            PreparedStatement stmt = c.prepareStatement("DELETE FROM salon.egzemplarz where id_egzemplarza=?");
+            stmt.setInt(1, id_unit);
+            int i = stmt.executeUpdate();
+            if(i!=1){
+                throw new ResponseStatusException(HttpStatus.CONFLICT, "Can't delete unit");
+            }
+        }
+        catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e.getCause());
+        }
+    }
+
+    @PostMapping("/update")
+    public void update(@RequestBody Map<String, String> request){
+        int id_unit;
+        String status;
+        try{
+            id_unit = Integer.parseInt(request.get("id_unit"));
+            status = request.get("status");
+        }
+        catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad request form");
+        }
+
+        try{
+            if(!getConn()){
+                throw new Exception();
+            }
+            PreparedStatement stmt = c.prepareStatement("UPDATE salon.egzemplarz set status = ? where id_egzemplarza = ?");
+            stmt.setString(1, status);
+            stmt.setInt(2, id_unit);
+            int i = stmt.executeUpdate();
+            if(i!=1){
+                throw new ResponseStatusException(HttpStatus.CONFLICT, "Unit does not exists");
+            }
+        }
+        catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Problem with connection with db");
+        }
+
+    }
 
 
 }
